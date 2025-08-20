ba6e9076fd8cbe9912e1c818aaa167e07432d801
David Naughton <nihiliad@gmail.com>
2025-08-20 15:55:58 -0500

    Adds ValidatedPMap

---
+++ pycific/validated.py
@@ -0,0 +1,80 @@
+from abc import ABC, abstractmethod
+from functools import cached_property
+from typing import Mapping
+
+from pydantic import BaseModel
+
+from pyrsistent import freeze, pmap, pvector
+from pyrsistent._pmap import PMap
+
+class ValidationError(ValueError):
+    pass
+
+class ValidatedPMap(PMap, ABC):
+    def __new__(cls, initial:Mapping={}, pre_size=0, *args, **kwargs):
+
+        # Ensure that all values within the initial Mapping are pyrsistent (immutable):
+        for k, v in initial.items():
+            initial[k] = freeze(v)
+
+        initial, buckets = cls._turbo_mapping(initial, pre_size)
+
+        #return PMap(len(initial), pvector().extend(buckets))
+        self = super().__new__(cls, len(initial), pvector().extend(buckets), *args, **kwargs)
+
+        try:
+            self._validate()
+            # TODO: Explain why we don't use the following, due to pyrsistent confusion in the case of validation errors.
+            # Also explain why we wanted to use it, to cahce the reuslt right away.
+            #valid = self.validated
+        # TODO: Explain why we don't use pydantic's validation error, which is because other pyrsistent or other things
+        # may also throw errors/exceptions. We just catch all of them and report that the attempt to validate failed.
+        #except ValidationError as ve: 
+        except Exception as e:
+            raise ValidationError('Attempt to validate input failed during ValidatedPMap instantiation')
+        return self
+
+    @abstractmethod
+    def _validate(self):
+        ...
+
+    @cached_property
+    def validated(self):
+        return self._validate()
+
+    @classmethod
+    def _turbo_mapping(cls, initial:Mapping={}, pre_size=0):
+        # TODO: Add explanation for copying and pasting this function from pyrsistent.
+        if pre_size:
+            size = pre_size
+        else:
+            try:
+                size = 2 * len(initial) or 8
+            except Exception:
+                # Guess we can't figure out the length. Give up on length hinting,
+                # we can always reallocate later.
+                size = 8
+
+        buckets = size * [None]
+
+        if not isinstance(initial, Mapping):
+            # Make a dictionary of the initial data if it isn't already,
+            # that will save us some job further down since we can assume no
+            # key collisions
+            initial = dict(initial)
+
+        for k, v in initial.items():
+            h = hash(k)
+            index = h % size
+            bucket = buckets[index]
+
+            if bucket:
+                bucket.append((k, v))
+            else:
+                buckets[index] = [(k, v)]
+
+        return (initial, buckets)
+
+# TODO: Add explanation for why we don't use strict=True
+class ValidatedPMapSpec(BaseModel, frozen=True, arbitrary_types_allowed=True):
+    pass
---
+++ tests/test_validated_pmap.py
@@ -0,0 +1,69 @@
+from typing import Mapping, Sequence
+
+import pytest
+
+from pycific.validated import ValidatedPMap, ValidatedPMapSpec, ValidationError
+#from pyrsistent.typing import PMap
+from pyrsistent._pmap import PMap
+from pyrsistent._pvector import PVector
+
+class ExampleMapValidated(ValidatedPMapSpec):
+    foo: str
+    bar: PVector
+    baz: PMap
+
+class ExampleMap(ValidatedPMap):
+    def _validate(self) -> ExampleMapValidated:
+        return ExampleMapValidated(
+            foo=self.foo,
+            bar=self.bar,
+            baz=self.baz,
+        )
+
+@pytest.fixture
+def example():
+    return ExampleMap({
+        'foo': 'manchoo',
+        'bar': ['the', 'door'],
+        'baz': {
+            'names': {'given': 'Mark', 'middle': ['Anthony'], 'nick': ['Baz'], 'surname': 'Luhrmann'},
+            'titles': ['AC'],
+            'movies': ['Strictly Ballroom', 'Romeo + Juliet', 'Moulin Rouge!'],
+        },
+        'extra': {'extra': 'Read all about it!'},
+    })
+
+def test_types(example):
+    assert isinstance(example, (PMap, ValidatedPMap, ExampleMap))
+    
+    assert isinstance(example.foo, str)
+
+    # Verify that nested values are also pyrsistent:
+    assert isinstance(example.bar, PVector)
+    assert isinstance(example.baz, PMap)
+    assert isinstance(example.baz.names, PMap)
+    assert isinstance(example.baz.names.middle, PVector)
+    assert isinstance(example.baz.names.nick, PVector)
+    assert isinstance(example.baz.movies, PVector)
+
+    assert isinstance(example.validated, ExampleMapValidated)
+
+    # Verify that the elements of the validated subset have the correct types:
+    assert isinstance(example.validated.foo, str)
+    assert isinstance(example.validated.bar, PVector)
+    assert isinstance(example.validated.baz, PMap)
+
+def test_inclusion_exclusion(example):
+    # Verify that the 'validated' property is excluded from the ValidatedPMap keys:
+    assert 'validated' not in example.keys()
+
+    # Verify that extra, unvalidated fields are included:
+    assert isinstance(example.extra, PMap)
+
+def test_missing_key():
+    with pytest.raises(ValidationError):
+        missing_key_example = ExampleMap({'foo': 'kung', 'bar': [1, 2, 3]})
+
+def test_bogus_value():
+    with pytest.raises(ValidationError):
+        bogus_value_example = ExampleMap({'foo': 'kung', 'bar': None, 'baz': {'profession': 'filmmaker'}})
