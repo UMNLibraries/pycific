956cb2912aebb19008b360c77eb521630f014ed7
David Naughton <nihiliad@gmail.com>
2025-08-25 09:14:24 -0500

    Improves naming in ValidatedStr tests

--- tests/test_validated_str.py
+++ tests/test_validated_str.py
@@ -4,25 +4,25 @@ import pytest
 
 from pycific.validated import ValidatedStr, ValidationError
 
-class ExampleStr(ValidatedStr):
+class NumericStr(ValidatedStr):
     def _validate(self) -> str:
         if not re.match(r'^\d{1,5}$', self):
-            raise ValueError(f'ExampleStr value {self} is invalid: must be one to five characters, all digits')
+            raise ValueError(f'NumericStr value {self} is invalid: must be one to five characters, all digits')
         return self
 
 def test_types():
-    example = ExampleStr('123')
-    for type in [str, ValidatedStr, ExampleStr]:
-        assert isinstance(example, type)
-    assert example == '123'
+    numstr = NumericStr('123')
+    for type in [str, ValidatedStr, NumericStr]:
+        assert isinstance(numstr, type)
+    assert numstr == '123'
 
 def test_int_input():
-    example = ExampleStr(123)
-    for type in [str, ValidatedStr, ExampleStr]:
-        assert isinstance(example, type)
-    assert example == '123'
-    assert not isinstance(example, int)
+    numstr = NumericStr(123)
+    for type in [str, ValidatedStr, NumericStr]:
+        assert isinstance(numstr, type)
+    assert numstr == '123'
+    assert not isinstance(numstr, int)
 
 def test_invalid_value():
     with pytest.raises(ValidationError):
-        invalid_value_example = ExampleStr('bogus')
+        NumericStr('bogus')
